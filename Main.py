0fe7fac930396ec22e6294b32d8c5978889c931f
Jui Tao Tsai <juitaotsai@Juis-MacBook-Pro.local>
2019-10-19 16:54:05 -0400

    requirements and script update

--- Main.py
+++ Main.py
@@ -4,19 +4,20 @@ from SpeechRecognition import speechToText
 from ParseText import sample_analyze_syntax
 
 # textFromSpeech = speechToText()
-noun_list = sample_analyze_syntax("states")
-dict = {}
-for noun in noun_list:
-    urls = findImageUrls(noun, 1)
-    for index, url in enumerate(urls):
-        labels = findObjectLabels(url)
-        print(labels)
-        if len(labels) != 0 and labels[0].lower() == noun.lower():
-            dict[noun] = url
-            break
-        elif index == len(urls) - 1:
-            dict[noun] = url
+def sendUrlDictThroughString(s):
+    noun_list = sample_analyze_syntax(s)
+    dict = {}
+    for noun in noun_list:
+        urls = findImageUrls(noun, 1)
+        for index, url in enumerate(urls):
+            labels = findObjectLabels(url)
+            if len(labels) != 0 and labels[0].lower() == noun.lower():
+                dict[noun] = url
+                break
+            elif index == len(urls) - 1:
+                dict[noun] = url
+    return dict
 
-##logic: find the image that resembles the noun the most,
-#if not -> Find the last image, or just not even store it!
-print(dict)
+# ##logic: find the image that resembles the noun the most,
+# #if not -> Find the last image, or just not even store it!
+# print(sendUrlDictThroughString("dog"))
